2f94d71361f4590b500eb5055ade47ecce8f4f5a
HectorLN <hector.lucero193@tectijuana.edu.mx>
2022-06-07 21:21:54 -0700

    Mis Tareas Part 1

--- AppTaskManagement/MisTareas.cs
+++ AppTaskManagement/MisTareas.cs
@@ -16,5 +16,45 @@ namespace AppTaskManagement
         {
             InitializeComponent();
         }
+
+        private void panelMenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+         
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void MisTareas_Load(object sender, EventArgs e)
+        {
+            cmbDate.Items.Add("Esta semana");
+            cmbDate.Items.Add("Proximas semanas");
+            cmbDate.Items.Add("Semanas anteriores");
+            cmbDate.Items.Add("Sin fecha de entrega");
+            cmbDate.Items.Add("Todas las tareas");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
